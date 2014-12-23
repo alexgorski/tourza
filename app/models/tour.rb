60604d3904c4e74cdfd7f456cc919a65c8e044cf
Alex Gorski <agorski@amnh.org>
2014-12-23 13:01:18 -0500

    Working Tour, Tag, Location models

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -2,7 +2,8 @@ class Tour
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  has_many :locations
+  has_and_belongs_to_many :locations
+  has_and_belongs_to_many :tags
 
   field :name,   type: String
   field :description,   type: String
